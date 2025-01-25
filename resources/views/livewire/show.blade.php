91791adf896fe8ee9ed0cb1c8c74c02edbcd1f24
EMshady92 <daniel24992@gmail.com>
2025-01-25 16:56:19 -0600

    Update show.blade.php

--- resources/views/livewire/show.blade.php
+++ resources/views/livewire/show.blade.php
@@ -139,6 +139,6 @@
                     </table>
                 </div>
             </div>
-        <button @click="open = false" class="bg-red-500 text-white px-4 py-2 rounded">Close Modal</button>
+        <button @click="open = false" class="bg-red-500 text-white px-4 py-2 rounded">Close</button>
     </div>
 </div>
